12a8e1828fee7d89606715576b16e8489bf062c8
John Hudson <jh@ingratexr.com>
2025-08-15 19:25:40 -0700

    add functions to take any filetype and return contents as text (if pdf or text) to avoid having to specify

--- src/scrimp/convert/convert.py
+++ src/scrimp/convert/convert.py
@@ -2,6 +2,7 @@ from pdf2image import convert_from_path
 import pytesseract
 import os
 
+PDF_MAGIC_HEADER = b"%PDF-"
 PAGE_BREAK_STR = "<PAGE_BREAK />"
 
 def pdf_to_text_ocr(filepath: str, discard_page_breaks: bool = False) -> str:
@@ -22,3 +23,33 @@ def pdf_to_text_ocr(filepath: str, discard_page_breaks: bool = False) -> str:
     texts = [pytesseract.image_to_string(page) for page in pages]
     delimiter = "\n\n" if discard_page_breaks else f"\n\n{PAGE_BREAK_STR}\n\n"
     return delimiter.join(texts)
+
+
+def get_from_pdf(filepath: str) -> str:
+    """
+    Placeholder - in the future extract text where possible; for now use ocr for
+    everything.
+    """
+    return pdf_to_text_ocr(filepath)
+
+
+def file_to_text(filepath: str) -> str:
+    """
+    Takes the path to a file and returns the contents of the file as plain text
+    if the file is supported (currently plain text filetypes or pdf). 
+    """
+    if not os.path.isfile(filepath):
+        raise Exception(f"{filepath} is not a file")
+    # If it has the pdf magic header, assume it's a pdf
+    with open(filepath, "rb") as f:
+        header = f.read(5)
+        if header == PDF_MAGIC_HEADER:
+            return get_from_pdf(filepath)
+    
+    # Otherwise try to get the plain text. If no worky, give up.
+    try:
+        with open(filepath, "r") as f:
+            return f.read()
+    except:
+        raise Exception(f"Does not compute! Could not decode {os.path.abspath(filepath)} as pdf or plain text.")
+
