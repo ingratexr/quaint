1499e4a2867a52c401047196a0ead867187d2f5a
John Hudson <jh@ingratexr.com>
2025-08-25 15:59:45 -0700

    add testing for core strutil functions; tweak strutil functions where necessary

--- src/quaint/core/strutil.py
+++ src/quaint/core/strutil.py
@@ -38,6 +38,9 @@ SCENE_TRANSITION_KEYWORDS=[
     "FADE TO",
     "FADE IN",
     "FADE OUT",
+    "SMASH TO",
+    "SMASH CUT TO",
+    "WHIP PAN TO",
     "DISSOLVE TO",
 ]
 
@@ -65,15 +68,16 @@ class Strutil:
         self,
         pages: list[str],
         max_chunk_chars: int=MAX_CHUNK_CHAR_COUNT,
+        join_str: str="\n\n",
     ) -> list[str]:
         """
         Takes a list of strings, concatenates them together, maintaining order
         and keeping each concatenated string no longer than max_chunk_chars
         long, and returns the new list.
 
-        ["ab", "cd", "ef"], 4 -> ["abcd", "ef"]
-        ["ab", "cd", "ef"], 3 -> ["ab", "cd", "ef"]
-        ["abc", "d", "e", "f"], 2 -> ["abc", "de", "f"]
+        ["ab", "cd", "ef"], 4, "" -> ["abcd", "ef"]
+        ["ab", "cd", "ef"], 3, "" -> ["ab", "cd", "ef"]
+        ["abc", "d", "e", "f"], 2, "" -> ["abc", "de", "f"]
 
         :param pages
         List of strings to chunk together.
@@ -82,14 +86,23 @@ class Strutil:
         Target max length of each string in returned array. Note that any 
         strings in the input array that are longer than this max will be kept in 
         order and returned as-is.
+
+        :param join_str
+        String to use to connect chunks. Does count towards number of charcters
+        in each chunk. Default is "\\n\\n"
         """
-        return self.chunk_strs_by_char_count(strs=pages, max_char_count=max_chunk_chars)
+        return self.chunk_strs_by_char_count(
+            strs=pages,
+            max_char_count=max_chunk_chars,
+            join_str=join_str,
+        )
 
 
     def chunk_screenplay_text(
         self,
         text: str,
         max_chunk_chars: int = MAX_CHUNK_CHAR_COUNT,
+        join_str="\n\n",
     ) -> list[str]:
         """
         Takes screenplay text as an input string and returns a list of strings
@@ -126,6 +139,7 @@ class Strutil:
         return self.chunk_strs_by_char_count(
             strs=chunks,
             max_char_count=max_chunk_chars,
+            join_str=join_str,
         )
 
 
@@ -133,6 +147,7 @@ class Strutil:
         self,
         text: str,
         max_chunk_chars: int = MAX_CHUNK_CHAR_COUNT,
+        join_str="\n\n",
     ) -> list[str]:
         """
         Takes generic text as an input string and returns a list of strings
@@ -164,6 +179,7 @@ class Strutil:
         return self.chunk_strs_by_char_count(
             strs=chunks,
             max_char_count=max_chunk_chars,
+            join_str=join_str,
         )
 
 
@@ -202,8 +218,7 @@ class Strutil:
                 if buff:
                     res.append(join_str.join(buff))
                     buff, buff_chars = [], 0
-                self.log(f"single string length ({s_len}) exceeds \
-                    max_char_count ({max_char_count})")
+                self.log(f"single string length ({s_len}) exceeds max_char_count ({max_char_count})")
                 res.append(s)
                 continue
 
@@ -244,7 +259,7 @@ class Strutil:
             # if it gets to the last line, add whatever the last chunk is
             if end == len(lines):
                 buff.append("\n".join(lines[start:len(lines)]))
-            # if end is a scene heading, add up to but not including that line
+            # if the last line matches, add up to but not including that line
             elif line_matcher(lines[end]):
                 buff.append("\n".join(lines[start:end]))
                 start = end
@@ -323,7 +338,7 @@ class Strutil:
         :param line
         String to check.
         """
-        l = line.strip().upper()
+        l = line.lstrip().upper()
         # if the line matches the keywords, it's a scene heading
         for k in SCENE_HEADING_KEYWORDS:
             # fountain scene headings don't have to be all caps
@@ -333,7 +348,7 @@ class Strutil:
         # in fountain, a period immediately followed by any aphanumeric
         # character forces a scene heading (ie ".ext" or ".some scene" but
         # not "...some scene") assume a match is a scene heading
-        return RE_FOUNTAIN_SCENE_HEADING.match(l)
+        return bool(RE_FOUNTAIN_SCENE_HEADING.match(l))
 
 
     def line_is_transition(self, line: str) -> bool:
@@ -353,7 +368,7 @@ class Strutil:
                 return True
 
         # in fountain, anything that ends with "TO:" is a transition
-        return line.endswith(" TO:") # use unstripped, for leading white space
+        return line.rstrip().upper().endswith(" TO:") # strip only right side
 
 
     def line_is_page_number(self, line: str) -> bool:
@@ -364,7 +379,7 @@ class Strutil:
         String to check.
         """
         l = line.strip()
-        return RE_PAGE_NUMBER.match(l) or RE_ROMAN_NUMERAL_PAGE_NUMBER.match(l)
+        return bool(RE_PAGE_NUMBER.match(l) or RE_ROMAN_NUMERAL_PAGE_NUMBER.match(l))
 
 
     def line_is_emptyish(self, line: str) -> bool:
@@ -378,7 +393,8 @@ class Strutil:
         :param line
         String to check.
         """
-        return line.strip() == "" or (len(line) == 1 and not line.isalnum())
+        l = line.strip()
+        return l == "" or (len(l) == 1 and not l.isalnum())
 
 
     def subarrayify(self, arr: list, subarray_length: int) -> list[str]:
